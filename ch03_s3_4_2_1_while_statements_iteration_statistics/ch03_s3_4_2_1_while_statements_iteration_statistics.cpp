2048b295e7ab2bfce8f242680ec57dd7838a113f
Cody Green <cody.william.green@gmai.com>
2025-12-27 04:24:43 -0800

    changes

--- ch03_s3_4_2_1_while_statements_iteration_statistics/ch03_s3_4_2_1_while_statements_iteration_statistics.cpp
+++ ch03_s3_4_2_1_while_statements_iteration_statistics/ch03_s3_4_2_1_while_statements_iteration_statistics.cpp
@@ -8,27 +8,18 @@
 // - Any non valid double type terminates the program.
 
 //TESTS:
-// - Input: 2 4 7 1 [Enter] [EOF]
+// - Input: 2 4 7 1 [EOF]
 //		Output: 
-//				Number of values read: 1
-//				Sum: 2
-//				Smallest : 2
-//				Largest : 2
-//				
-//				Number of values read : 2
-//				Sum : 6
-//				Smallest : 2
-//				Largest : 4
-//				
-//				Number of values read : 3
-//				Sum : 13
-//				Smallest : 2
-//				Largest : 7
-//				
-//				Number of values read : 4
-//				Sum : 14
+//				Number of values read: 4
+//				Sum: 14
 //				Smallest : 1
 //				Largest : 7
+// - Input: 420.69 -69 42.69 69.42 [EOF]
+//		Output: 
+//				Number of values read: 4
+//				Sum: 463.8
+//				Smallest : -69
+//				Largest : 420.69
 
 #include "PPP.h"
 
@@ -40,37 +31,42 @@ int main()
 	double smallest_input{ 0 };
 	double largest_input{ 0 };
 
-	std::cout << "Any non-number terminates the program.\n" 
-		<< "Usage: Input a series of floating-point numbers, press [Enter] to continue.\n"
-		<< "INPUT: ";
+	std::cout << "Usage: Input a series of numbers (space/newline separated).\nEnd with EOF.\n=> ";
 
 	while (std::cin >> user_input)
 	{
 
 		if (input_count == 0)
 		{
-			smallest_input = user_input;
-			largest_input = user_input;
+			smallest_input = largest_input = user_input;
+		}
+		else
+		{
+			if (user_input < smallest_input)
+			{
+				smallest_input = user_input;
+			}
+
+			if (user_input > largest_input)
+			{
+				largest_input = user_input;
+			}
 		}
 
 		input_count++;
 		sum += user_input;
-
-		if (user_input >= largest_input)
-		{
-			largest_input = user_input;
-		}
-		
-		if (user_input <= smallest_input)
-		{
-			smallest_input = user_input;
-		}
-		std::cout <<
-			"Number of values read: " << input_count << '\n'
-			<< "Sum: " << sum << '\n'
-			<< "Smallest:  " << smallest_input << '\n'
-			<< "Largest: " << largest_input << "\n\n";
 	}
+
+	if (input_count == 0)
+	{
+		std::cerr << "Error: No input was given!\n\n";
+	}
+
+	std::cout <<
+		"\nNumber of values read: " << input_count << '\n'
+		<< "Sum: " << sum << '\n'
+		<< "Smallest:  " << smallest_input << '\n'
+		<< "Largest: " << largest_input << "\n\n";
 
 	return 0;
 }
